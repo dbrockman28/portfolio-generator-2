0488cd0c04a8fa091066ca3210b1a131b4711f24
Rowdy Goat <dbrockman28@gmail.com>
2021-01-12 20:00:34 -0600

    finished validation

--- app.js
+++ app.js
@@ -38,17 +38,32 @@ const promptUser = () => {
       }
     },
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to include an about section?',
+      default: true
+    },
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself'
+      message: 'Provide some information about yourself',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          return false;
+        }
+      }
     }
   ])
 };
 
 const promptProject = portfolioData => {
+
   if (!portfolioData.projects) {
     portfolioData.projects = [];
   }
+  
   console.log(`
   =================
   Add a New Project
